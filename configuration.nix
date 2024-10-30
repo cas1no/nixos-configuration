a5443005b287090bc0b695216ea0a27566c5efa7
Ilya Vinski <44465831+cas1no@users.noreply.github.com>
2024-10-30 21:56:21 +0100

    Disable unwanted plugins of blueman

--- configuration.nix
+++ configuration.nix
@@ -125,7 +125,10 @@
       package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
       modesetting.enable = true;
       prime = {
-        offload.enable = true;
+        offload = {
+	  enable = true;
+          enableOffloadCmd = true;
+	};
         nvidiaBusId = "PCI:1:0:0";
         intelBusId = "PCI:0:2:0";
       };
@@ -164,47 +167,51 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs;
-    let
-      nvidia-offload = writeShellScriptBin "nvidia-offload" ''
-        export __NV_PRIME_RENDER_OFFLOAD=1
-        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-        export __GLX_VENDOR_LIBRARY_NAME=nvidia
-        export __VK_LAYER_NV_optimus=NVIDIA_only
-        exec -a "$0" "$@"
-      '';
-    in
-    [
-      nvidia-offload
-      tdesktop
-      git
-      wget
-      firefox
-      (chromium.override {
-        enableWideVine = true;
-      })
-      qbittorrent
-      krita
-      smplayer
-      mpv
-      pass
-      gcc
-      gnumake
-      p7zip
-      ripgrep
-      discord
-      xclip
-      (calibre.override {
-        unrarSupport = true;
-      })
-      valgrind
-    ];
+  environment.systemPackages = with pkgs; [
+    tdesktop
+    git
+    wget
+    firefox
+    (chromium.override {
+      enableWideVine = true;
+    })
+    qbittorrent
+    krita
+    smplayer
+    mpv
+    pass
+    gcc
+    gnumake
+    p7zip
+    ripgrep
+    discord
+    xclip
+    (calibre.override {
+      unrarSupport = true;
+    })
+    valgrind
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs = {
     # Enable network manager applet
     nm-applet.enable = true;
+
+    # Disable unwanted plugins of blueman
+    dconf = {
+      enable = true;
+      profiles.user.databases = [
+        {
+          lockAll = true; # prevents overriding
+          settings = {
+            "org/blueman/general" = {
+              plugin-list = [ "!ConnectionNotifier" ];
+            };
+          };
+        }
+      ];
+    };
 
     steam.enable = true;
 
